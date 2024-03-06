0fb8235e938b32e0419276dedff2893fe7360bf7
samfelix61 <samfelix61@gmail.com>
2024-03-06 08:31:31 +0300

    create index.html

---
+++ samfelix personal project/index.js
@@ -0,0 +1,9 @@
+//console.log(`Hello`);
+//console.log(`I like pizza`);
+
+document.getElementById("myH1").textcontent =`Hello are you`;
+
+
+//windows.alert(`this is an alert`);
+
+// this is a comment 
